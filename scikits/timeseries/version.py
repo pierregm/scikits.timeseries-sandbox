329194f28662dfc4ddcae738eb0c27e3bd40655f
mattknox_ca <mattknox_ca@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
2008-06-17 00:49:21 +0000

    hardcoded __version__ for now so that the module will import until version jiggery pokery is sorted out

--- scikits/timeseries/version.py
+++ scikits/timeseries/version.py
@@ -18,5 +18,7 @@
 #
 #__all__ = str(version)
 
-from pkg_resources import require
-__version__ = require('scikits.timeseries')[0].version
+#from pkg_resources import require
+#__version__ = require('scikits.timeseries')[0].version
+
+__version__ = '0.67.0'
